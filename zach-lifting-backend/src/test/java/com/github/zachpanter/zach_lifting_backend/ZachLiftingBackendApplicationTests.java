3199dbefdef8e12a42ce772d7485e2385d7e70d1
Zach Panter <zooloowhiskee@gmail.com>
2025-05-31 17:18:12 -0600

    Spring Initialzer template with JPA and Web

---
+++ zach-lifting-backend/src/test/java/com/github/zachpanter/zach_lifting_backend/ZachLiftingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zachpanter.zach_lifting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZachLiftingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
